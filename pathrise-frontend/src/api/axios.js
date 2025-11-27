9f58e389507f9a8940a9228cff7f1dd855ab4005
Nitin-0017 <raonitinkumar06@gmail.com>
2025-11-27 17:18:45 +0530

    axios file

--- pathrise-frontend/src/api/axios.js
+++ pathrise-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pathrise-project.onrender.com",
+  baseURL: "http://localhost:3000/api",
 });
 
 API.interceptors.request.use((config) => {
